0ce7a4b44984ac14c7a4f8d1f14bd66cc083920f
Diego Diaz Rodas <diegodiazrodas@gmail.com>
2022-05-19 23:18:09 -0300

    desafio proyecto 2

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ let formulario = document.getElementById("formulario");
 let botonEnviar = document.getElementById("btnPrincipal");
 let nombEmpleado = document.getElementById("nombEmpleado");
 let apellEmpleado = document.getElementById("apellEmpleado");
+let edadEmpleado = document.getElementById("edadEmpleado");
 let seniorityEmp = document.getElementById("seniorityEmp");
 let horasEmp = document.getElementById("horasEmp");
 let valorHoraEmp = document.getElementById("valorHoraEmp");
+let divMen30 = document.getElementById("divMen30");
+let btnMen30 = document.getElementById("btnMen30");
+let btnMay30 = document.getElementById("btnMay30");
 
 navbar.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="container-fluid">
@@ -16,93 +20,115 @@ navbar.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
       <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="#">Home</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-      </li>
     </ul>
   </div>
 </div>
 </nav>`;
 
-header.innerHTML = `<h1 class="text-center pt-5 display-2si">Bienvenidos, esto será la entrega del desafío</h1>`;
+header.innerHTML = `<h1 class="text-center pt-5 display-5">Bienvenido/a, por favor ingrese la carga de los nuevos empleados</h1>`;
 
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-  crearEmpleado();
-  mostrarEnTabla();
-  console.log("asdasd");
-});
+let empleados = [];
 
-const empleados = [];
+const obtenerEmpleadoLocalStorage = () => {
+    if (localStorage.getItem("listaEmpleados")) {
+        empleados = JSON.parse(localStorage.getItem("listaEmpleados"));
+    }
+};
+
+obtenerEmpleadoLocalStorage();
+
 let nuevoEmpleado = true;
 
 const bonos = {
-  Senior: 1.9,
-  "Semi Senior": 1.5,
-  Junior: 1.1,
-  default: 1,
+    Senior: 1.9,
+    "Semi Senior": 1.5,
+    Junior: 1.1,
+    default: 1,
 };
 
 const crearEmpleado = () => {
-  let empleado = {
-    nombre: nombEmpleado.value,
-    apellido: apellEmpleado.value,
-    seniority: seniorityEmp.value,
-    horasTrabajadas: horasEmp.value,
-    valorHora: valorHoraEmp.value,
-    salario: 0,
-  };
+    let empleado = {
+        nombre: nombEmpleado.value,
+        apellido: apellEmpleado.value,
+        edad: edadEmpleado.value,
+        seniority: seniorityEmp.value,
+        horasTrabajadas: horasEmp.value,
+        valorHora: valorHoraEmp.value,
+        salario: 0,
+    };
 
-  empleados.push(empleado);
+    empleados.push(empleado);
 
-  formulario.reset();
+    formulario.reset();
 };
 
 const calcularSalario = (empleado) => {
-  if (empleado.horasTrabajadas > 200) {
-    empleado.salario =
-      (empleado.horasTrabajadas * empleado.valorHora + 10000) *
-      (bonos[empleado.seniority] || bonos["default"]);
-  } else if (empleado.horasTrabajadas > 180) {
-    empleado.salario =
-      (empleado.horasTrabajadas * empleado.valorHora + 5000) *
-      (bonos[empleado.seniority] || bonos["default"]);
-  } else {
-    empleado.salario =
-      empleado.horasTrabajadas *
-      empleado.valorHora *
-      (bonos[empleado.seniority] || bonos["default"]);
-  }
+    if (empleado.horasTrabajadas > 200) {
+        empleado.salario =
+            (empleado.horasTrabajadas * empleado.valorHora + 10000) *
+            (bonos[empleado.seniority] || bonos["default"]);
+    } else if (empleado.horasTrabajadas > 180) {
+        empleado.salario =
+            (empleado.horasTrabajadas * empleado.valorHora + 5000) *
+            (bonos[empleado.seniority] || bonos["default"]);
+    } else {
+        empleado.salario =
+            empleado.horasTrabajadas *
+            empleado.valorHora *
+            (bonos[empleado.seniority] || bonos["default"]);
+    }
 };
 
-const mostrarEnTabla = () => {
-  let idAcomulador = 0;
+const guardarEmpleados = (clave, valor) => {
+    localStorage.setItem(clave, valor);
+};
 
-  tablaEmpleados.innerHTML = "";
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    crearEmpleado();
+    guardarEmpleados("listaEmpleados", JSON.stringify(empleados));
+});
 
-  empleados.forEach((empleado) => {
-    calcularSalario(empleado);
 
-    tablaEmpleados.innerHTML += `<tr>
+const filtroDeEdades = (edades) => {
+    let resultadoFiltroEmpleados;
+    if (edades === 'mayores') {
+        resultadoFiltroEmpleados = empleados.filter((i) => i.edad > 30);
+    } else {
+        resultadoFiltroEmpleados = empleados.filter((i) => i.edad <= 30);
+    }
+
+    return resultadoFiltroEmpleados;
+};
+
+btnMen30.addEventListener("click", (e) => {
+    e.preventDefault();
+    mostrarEnTabla(filtroDeEdades("menores"));
+});
+
+btnMay30.addEventListener("click", (e) => {
+    e.preventDefault();
+    mostrarEnTabla(filtroDeEdades("mayores"));
+});
+
+const mostrarEnTabla = (filtradoMen30) => {
+    let idAcomulador = 0;
+
+    tablaEmpleados.innerHTML = "";
+
+    filtradoMen30.forEach((empleado) => {
+        calcularSalario(empleado);
+
+        tablaEmpleados.innerHTML += `<tr>
         <th scope="row">${idAcomulador++}</th>
         <td>${empleado.nombre}</td>
         <td>${empleado.apellido}</td>
+        <td>${empleado.edad}</td>
         <td>${empleado.seniority}</td>
         <td>AR$ ${empleado.salario}</td>
     </tr>
     `;
-  });
+    });
 };
 
-console.table(empleados);
+// console.table(empleados);
